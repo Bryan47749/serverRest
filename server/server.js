124d680465440d306dc1cca8ec705a0dda8e4249
Toalumbo Rodriguez Bryan David <btoalumbo7749@uta.edu.ec>
2020-01-25 19:51:02 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('../config/config');
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.listen(process.env.PORT, () => {
+    console.log('Puerto:', process.env.PORT);
+});
